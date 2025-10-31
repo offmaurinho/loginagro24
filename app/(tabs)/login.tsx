6874e0dbd26423385f380ae39de936dc60fdc199
Fabricio Mauro <fabricio_mauro@live.com>
2025-10-30 21:38:22 -0400

    tela de login

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,17 +1,82 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button, TextInput} from 'react-native';
+import { supabase  } from '@/src/supabaseClient';
+import Toast from 'react-native-toast-message';
+
+import { useState } from 'react';
 
 export default function Login(){
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const autenticar = async () => {
+        Toast.show({
+            type: 'success',
+            text1: 'Sucesso',
+            text2: 'Login efetuado com sucesso!'
+        })
+    }
+
     return(
         <View style={style.container}>
-            <Text>
-                Só alegria!
-            </Text>
+            <View style={style.card}>
+        <Text style={style.titulo}> Autenticar </Text> 
+        <TextInput 
+          style={style.caixaText} 
+          placeholder='Informe seu Email' 
+          value={email} 
+          onChangeText={setEmail} 
+        /> 
+        <TextInput 
+          style={style.caixaText} 
+          placeholder='Informe sua senha' 
+          value={senha} 
+          onChangeText={setSenha} 
+          secureTextEntry
+        />
+        <View style={style.botao}>
+            <Button 
+            title='Login' 
+            onPress={autenticar}
+            color = '#5CBD0F'
+            />
         </View>
+      </View>
+      <Toast /> 
+    </View> 
     )
 }
 
 const style = StyleSheet.create({
     container: {
         flex:1,
+        backgroundColor: '#D9D9D9',
+        justifyContent: 'center',
+        padding: 20,
+        alignItems: 'center'
+    },
+    card:{
+        width: '40%',
+    },
+    titulo:{
+        fontSize: 25,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#000',
+        marginBottom: 15
+    },
+    caixaText:{
+        backgroundColor: '#fff',
+        color: '#000',
+        padding: 10,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: '#c8d537ff',
+        marginBottom: 10,
+        borderRadius: 5
+    },
+    botao:{
+        width: '50%',
+        alignSelf: 'center',
+        marginTop: 10
     }
 })
--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -4,5 +4,15 @@ import { createClient } from "@supabase/supabase-js";
 //Lendo as variavis do .env
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabasePublishableKey = supabaseAnonKey;
+//export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
+    auth: {
+      storage: typeof window !== "undefined" ? window.localStorage : undefined,
+      //storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  })
